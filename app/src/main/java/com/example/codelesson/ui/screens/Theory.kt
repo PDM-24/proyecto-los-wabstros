e22daa9b6f6af2dbc4fa4826257b61dc96788e97
Florence196 <00361222@uca.edu.sv>
2024-05-10 22:11:44 -0600

    add: base screens

---
+++ app/src/main/java/com/example/codelesson/ui/screens/Theory.kt
@@ -0,0 +1,9 @@
+package com.example.codelesson.ui.screens
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.runtime.Composable
+
+@Composable
+fun Theory (innerPadding: PaddingValues){
+
+}

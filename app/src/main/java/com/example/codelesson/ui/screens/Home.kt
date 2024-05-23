33a8c154fabcac217a0b7c6800dd3c090512b2b2
EliasZelaya <00146322@uca.edu.sv>
2024-05-23 11:25:44 -0600

    Add: Start homescreen

--- app/src/main/java/com/example/codelesson/ui/screens/Home.kt
+++ app/src/main/java/com/example/codelesson/ui/screens/Home.kt
@@ -1,9 +1,19 @@
 package com.example.codelesson.ui.screens
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 
 @Composable
 fun Home (innerPadding: PaddingValues){
+    Scaffold(
+        topBar = {
 
+        },
+        bottomBar = {
+
+        }
+    ) { InnerPadding ->
+
+    }
 }

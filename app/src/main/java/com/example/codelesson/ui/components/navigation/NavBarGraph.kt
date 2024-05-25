939f58dba0c5c906da86ed34efe56c786dd52976
EliasZelaya <00146322@uca.edu.sv>
2024-05-25 00:35:38 -0600

    Add: Screenoutes and graphs

---
+++ app/src/main/java/com/example/codelesson/ui/components/navigation/NavBarGraph.kt
@@ -0,0 +1,9 @@
+package com.example.codelesson.ui.components.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun NavBarGraph() {
+    val navController = rememberNavController()
+}
--- app/src/main/java/com/example/codelesson/ui/components/navigation/ScreenRoute.kt
+++ app/src/main/java/com/example/codelesson/ui/components/navigation/ScreenRoute.kt
@@ -1,4 +1,16 @@
 package com.example.codelesson.ui.components.navigation
 
-class ScreenRoute {
+sealed class Graph(val graph: String) {
+    data object LOGIN : Graph("LOGIN")
+    data object HOME : Graph("HOME")
+}
+
+sealed class LoginGraph(val route: String) {
+    data object Login: LoginGraph("Login")
+    data object SignOn: LoginGraph("SignOn")
+}
+
+sealed class HomeGraph(val route: String) {
+    data object Home: HomeGraph("Home")
+    data object Theory: HomeGraph("Theory")
 }

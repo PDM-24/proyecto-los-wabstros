84ba3f57e7457534c93f8cc57e2a45e245e9c98c
EliasZelaya <00146322@uca.edu.sv>
2024-05-26 23:11:41 -0600

    Add: NavBarGraph to startdestination for homescreen

--- app/src/main/java/com/example/codelesson/MainActivity.kt
+++ app/src/main/java/com/example/codelesson/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.codelesson
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.example.codelesson.ui.screens.Main
 import com.example.codelesson.ui.theme.CodeLessonTheme
 
 class MainActivity : ComponentActivity() {
@@ -11,6 +12,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CodeLessonTheme {
                 {/* TO DO */}
+                Main()
             }
         }
     }

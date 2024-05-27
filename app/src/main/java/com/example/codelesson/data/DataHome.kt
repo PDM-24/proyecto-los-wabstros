def6bc6618f6453f1477a2232f16a0ba2d89be93
EliasZelaya <00146322@uca.edu.sv>
2024-05-26 23:05:53 -0600

    Add: Data for Home component

--- app/src/main/java/com/example/codelesson/data/DataHome.kt
+++ app/src/main/java/com/example/codelesson/data/DataHome.kt
@@ -1,5 +1,13 @@
 package com.example.codelesson.data
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.ui.graphics.vector.ImageVector
+import com.example.codelesson.ui.components.navigation.HomeGraph
+
 data class DataHome(
     val name: String
 )
@@ -29,6 +37,34 @@ fun getName(): List<DataHome> {
         ),
         DataHome(
             "OPERADORES"
+        )
+    )
+}
+
+data class BottomNavItems(
+    val title: String,
+    val description: String,
+    val iconDefault: ImageVector,
+    val iconSelected: ImageVector,
+    val route: String,
+)
+
+fun NavItemsList(): List<BottomNavItems> {
+    return listOf(
+        BottomNavItems
+            (
+            "Desarrollador C++",
+            "Home",
+            Icons.Outlined.Home,
+            Icons.Filled.Home,
+            HomeGraph.Home.route
+        ),
+        BottomNavItems(
+            "Profile",
+            "Profile",
+            Icons.Outlined.Person,
+            Icons.Filled.Person,
+            HomeGraph.Profile.route
         )
     )
 }

bdf7f9559b9f544aa37e78f1bf16a99d718924e3
EliasZelaya <00146322@uca.edu.sv>
2024-05-26 23:13:27 -0600

    make navigation

--- app/src/main/java/com/example/codelesson/ui/components/navigation/NavBarGraph.kt
+++ app/src/main/java/com/example/codelesson/ui/components/navigation/NavBarGraph.kt
@@ -1,9 +1,32 @@
 package com.example.codelesson.ui.components.navigation
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.runtime.Composable
-import androidx.navigation.compose.rememberNavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.codelesson.ui.screens.Home
+import com.example.codelesson.ui.screens.Profile
+import com.example.codelesson.ui.screens.Theory
 
 @Composable
-fun NavBarGraph() {
-    val navController = rememberNavController()
+fun NavBarGraph(innerPadding: PaddingValues, navController: NavHostController) {
+
+    NavHost(
+        navController = navController,
+        route = Graph.HOME.graph,
+        startDestination = HomeGraph.Home.route
+    ) {
+        composable(HomeGraph.Home.route) {
+            Home(innerPadding, navController)
+        }
+
+        composable(HomeGraph.Profile.route) {
+            Profile(innerPadding)
+        }
+
+        composable(HomeGraph.Theory.route) {
+            Theory(innerPadding)
+        }
+    }
 }

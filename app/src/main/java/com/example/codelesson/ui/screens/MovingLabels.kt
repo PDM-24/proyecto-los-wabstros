c6f4805331f1b994d32723f1ee6fcbc7a70309a8
EliasZelaya <00146322@uca.edu.sv>
2024-06-13 01:30:36 -0600

    Star to create screen moving labels

--- app/src/main/java/com/example/codelesson/ui/screens/MovingLabels.kt
+++ app/src/main/java/com/example/codelesson/ui/screens/MovingLabels.kt
@@ -1,10 +1,45 @@
 package com.example.codelesson.ui.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.codelesson.ui.components.movinglabelcomponents.LabelEmpty
+import com.example.codelesson.ui.components.movinglabelcomponents.QuestionLabel
+import com.example.codelesson.ui.components.practicecomponents.CodeBlock
+import com.example.codelesson.ui.components.practicecomponents.ShortIndication
 import com.example.codelesson.util.PracticeViewModel
 
 @Composable
 fun MovingLabels (innerPadding: PaddingValues, viewModel: PracticeViewModel){
+    Column(
+        modifier = Modifier
+            .padding(innerPadding)
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Center
+    ) {
+        CodeBlock(code = "INT ES UNA VARIABLE DE TIPO ENTERO CON UN RANGO DE -32768 A 32767.")
+        ShortIndication(indication = "Encuentre la definicion")
 
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceAround
+        ) {
+            QuestionLabel()
+            LabelEmpty()
+        }
+    }
+}
+
+@Preview(showSystemUi = true)
+@Composable
+private fun MovingLabelPreview() {
+    MovingLabels(innerPadding = PaddingValues(), viewModel = PracticeViewModel())
 }

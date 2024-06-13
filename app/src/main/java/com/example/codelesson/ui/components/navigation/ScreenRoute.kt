92d9b3a557930976db5d76ee958cb58827c47092
EliasZelaya <00146322@uca.edu.sv>
2024-06-12 23:33:56 -0600

    Add nested navigation

--- app/src/main/java/com/example/codelesson/ui/components/navigation/ScreenRoute.kt
+++ app/src/main/java/com/example/codelesson/ui/components/navigation/ScreenRoute.kt
@@ -1,7 +1,9 @@
 package com.example.codelesson.ui.components.navigation
 
 sealed class Graph(val graph: String) {
+    data object ROOT : Graph("ROOT")
     data object LOGIN : Graph("LOGIN")
+    data object PROFILE : Graph("PROFILE")
     data object HOME : Graph("HOME")
     data object QUIZ : Graph("QUIZ")
 }
@@ -13,14 +15,18 @@ sealed class LoginGraph(val route: String) {
 
 sealed class HomeGraph(val route: String) {
     data object Home: HomeGraph("Home")
-    data object Profile: HomeGraph("Profile")
 }
 
-sealed class Profile(val route: String) {
-
+sealed class ProfileGraph(val route: String) {
+    data object Profile: HomeGraph("Profile")
+    data object Security: ProfileGraph("Security")
+    data object EditProfile: ProfileGraph("EditProfile")
 }
 
 sealed class QuizGraph(val route: String) {
     data object Theory: QuizGraph("Theory")
-    data object Quiz: QuizGraph("Quiz")
+    data object MultipleResponse: QuizGraph("MultipleResponse")
+    data object MovingLabel: QuizGraph("MovingLabel")
+    data object ResponseEntry: QuizGraph("ResponseEntry")
+    data object LessonRecap: QuizGraph("LessonRecap")
 }

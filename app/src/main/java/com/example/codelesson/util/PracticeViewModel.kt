285831b43ecaa6f48d9a319e55357307651ae65a
EliasZelaya <00146322@uca.edu.sv>
2024-06-14 02:37:33 -0600

    Viewmodel

--- app/src/main/java/com/example/codelesson/util/PracticeViewModel.kt
+++ app/src/main/java/com/example/codelesson/util/PracticeViewModel.kt
@@ -1,14 +1,20 @@
 package com.example.codelesson.util
 
+import androidx.compose.ui.geometry.Offset
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-class PracticeViewModel : ViewModel() {
-    private val _titleTopBar = MutableStateFlow("Desarrollador C++")
-    val titleTopBar = _titleTopBar.asStateFlow()
+    class PracticeViewModel : ViewModel() {
+        private val _titleTopBar = MutableStateFlow("Desarrollador C++")
+        val titleTopBar = _titleTopBar.asStateFlow()
+        private val _emptyLabelPosition = MutableStateFlow(Offset.Zero)
+        val emptyLabelPosition = _emptyLabelPosition.asStateFlow()
 
-    fun setTitle(title: String) {
-        _titleTopBar.value = title
+        fun getEmptyLabelPosition(position: Offset) {
+            _emptyLabelPosition.value = position
+        }
+        fun setTitle(title: String) {
+            _titleTopBar.value = title
+        }
     }
-}

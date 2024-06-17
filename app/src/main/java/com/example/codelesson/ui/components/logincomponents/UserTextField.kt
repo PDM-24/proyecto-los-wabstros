505c90a03036be73215fb3c5a840dbc269bbf6ca
Alison Argueta <00076422@uca.edu.sv>
2024-06-17 16:05:21 -0600

    :memo: Add: Component TextField user

---
+++ app/src/main/java/com/example/codelesson/ui/components/logincomponents/UserTextField.kt
@@ -0,0 +1,64 @@
+package com.example.codelesson.ui.components.logincomponents
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusDirection
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.codelesson.R
+import com.example.codelesson.ui.theme.poppins
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UserTextField(
+    value: String,
+    hint: String,
+){
+    var variable by remember { mutableStateOf("") }
+
+    TextField(
+        value = variable,
+        onValueChange = { variable = it },
+        placeholder = {
+            Text(text = hint,
+                style = TextStyle(
+                    fontSize = 18.sp,
+                    fontFamily = poppins,
+                    color = Color(0xFFB6B6B6),
+                )
+            )
+        },
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 20.dp)
+            .clip(RoundedCornerShape(percent = 25)),
+        colors = TextFieldDefaults.textFieldColors(
+            containerColor = Color(0xFF8E8E8E),
+            focusedIndicatorColor = Color(0xFFFFFFFF)
+        )
+    )
+}
--- app/src/main/java/com/example/codelesson/util/PracticeViewModel.kt
+++ app/src/main/java/com/example/codelesson/util/PracticeViewModel.kt
@@ -1,15 +1,12 @@
 package com.example.codelesson.util
 
-import androidx.compose.ui.geometry.Offset
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 
-    class PracticeViewModel : ViewModel() {
-        private val _titleTopBar = MutableStateFlow("Desarrollador C++")
-        val titleTopBar = _titleTopBar.asStateFlow()
-        private val _emptyLabelPosition = MutableStateFlow(Offset.Zero)
-        val emptyLabelPosition = _emptyLabelPosition.asStateFlow()
+class PracticeViewModel : ViewModel() {
+    private val _titleTopBar = MutableStateFlow("Desarrollador C++")
+    val titleTopBar = _titleTopBar.asStateFlow()
 
     fun setTitle(title: String) {
         _titleTopBar.value = title
@@ -17,7 +14,3 @@ import kotlinx.coroutines.flow.asStateFlow
     fun VerifyingAnswer(answer: String, correctAnswer: String) =
         answer == correctAnswer
 }
-        fun getEmptyLabelPosition(position: Offset) {
-            _emptyLabelPosition.value = position
-        }
-    }

4f56a71cb62ae6734017595bf08943c25f95df8f
Alison Argueta <00076422@uca.edu.sv>
2024-06-17 16:09:08 -0600

    :fire: Update: Removing import

--- app/src/main/java/com/example/codelesson/ui/components/logincomponents/UserTextField.kt
+++ app/src/main/java/com/example/codelesson/ui/components/logincomponents/UserTextField.kt
@@ -3,13 +3,7 @@ package com.example.codelesson.ui.components.logincomponents
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
@@ -20,16 +14,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalFocusManager
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.codelesson.R
 import com.example.codelesson.ui.theme.poppins
 
 @OptIn(ExperimentalMaterial3Api::class)

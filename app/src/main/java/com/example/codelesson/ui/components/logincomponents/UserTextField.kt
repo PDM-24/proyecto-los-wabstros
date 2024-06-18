bb0045b6957518d3b30cee7bfe0247441e6aef38
Alison Argueta <00076422@uca.edu.sv>
2024-06-18 17:36:24 -0600

    :lipstick: Add: strings.xml and logo png

--- app/src/main/java/com/example/codelesson/ui/components/logincomponents/UserTextField.kt
+++ app/src/main/java/com/example/codelesson/ui/components/logincomponents/UserTextField.kt
@@ -3,6 +3,8 @@ package com.example.codelesson.ui.components.logincomponents
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -14,8 +16,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusDirection
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.codelesson.ui.theme.poppins
@@ -24,13 +29,14 @@ import com.example.codelesson.ui.theme.poppins
 @Composable
 fun UserTextField(
     value: String,
+    onChange: (String) -> Unit,
     hint: String,
+    modifier: Modifier = Modifier
 ){
-    var variable by remember { mutableStateOf("") }
-
+    val focusManager = LocalFocusManager.current
     TextField(
-        value = variable,
-        onValueChange = { variable = it },
+        value = value,
+        onValueChange = onChange,
         placeholder = {
             Text(text = hint,
                 style = TextStyle(
@@ -44,6 +50,10 @@ fun UserTextField(
             .fillMaxWidth()
             .padding(bottom = 20.dp)
             .clip(RoundedCornerShape(percent = 25)),
+        keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+        keyboardActions = KeyboardActions(
+            onNext = { focusManager.moveFocus(FocusDirection.Down) }
+        ),
         colors = TextFieldDefaults.textFieldColors(
             containerColor = Color(0xFF8E8E8E),
             focusedIndicatorColor = Color(0xFFFFFFFF)

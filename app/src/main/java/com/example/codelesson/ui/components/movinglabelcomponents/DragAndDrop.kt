ec4b672dfb8a7161623829df2d30504edd5d375c
EliasZelaya <00146322@uca.edu.sv>
2024-06-21 08:51:22 -0600

    Add components to moving label and DragAndDrop

--- app/src/main/java/com/example/codelesson/ui/components/movinglabelcomponents/DragAndDrop.kt
+++ app/src/main/java/com/example/codelesson/ui/components/movinglabelcomponents/DragAndDrop.kt
@@ -1,8 +1,12 @@
 package com.example.codelesson.ui.components.movinglabelcomponents
-
-import androidx.compose.foundation.gestures.detectDragGesturesAfterLongPress
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -10,8 +14,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.boundsInWindow
 import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import com.example.codelesson.util.PracticeViewModel
 
 internal val LocalDragTargetInfo = compositionLocalOf { DragAndDrop() }
 
@@ -19,8 +28,13 @@ internal val LocalDragTargetInfo = compositionLocalOf { DragAndDrop() }
 fun <T> DragTarget(
     modifier: Modifier = Modifier,
     dataDrop: T,
+    viewModel: PracticeViewModel,
     content: @Composable (() -> Unit),
 ) {
+    val isDragging = remember {
+        mutableStateOf(false)
+    }
+
     var currentPosition by remember {
         mutableStateOf(Offset.Zero)
     }
@@ -28,14 +42,16 @@ fun <T> DragTarget(
     val currentState = LocalDragTargetInfo.current
 
     Box(
-        modifier = Modifier
+        modifier = modifier
             .onGloballyPositioned {
                 currentPosition = it.localToWindow(Offset.Zero)
             }
             .pointerInput(Unit) {
-                detectDragGesturesAfterLongPress(
+                detectDragGestures(
                     onDragStart = {
+                        viewModel.startDragging()
                         currentState.dataDrop = dataDrop
+                        isDragging.value = true
                         currentState.isDragging = true
                         currentState.dragPosition = currentPosition + it
                         currentState.draggableComposable = content
@@ -45,19 +61,97 @@ fun <T> DragTarget(
                         currentState.dragOffset += Offset(dragAmount.x, dragAmount.y)
                     },
                     onDragEnd = {
+                        viewModel.stopDragging()
                         currentState.dragOffset = Offset.Zero
                         currentState.isDragging = false
+                        isDragging.value = false
                     },
                     onDragCancel = {
+                        viewModel.stopDragging()
                         currentState.dragOffset = Offset.Zero
                         currentState.isDragging = false
+                        isDragging.value = false
                     }
                 )
             }
-    )
+    ) {
+        if (!isDragging.value) {
+            content()
+        }
+    }
 }
 
-internal class DragAndDrop {
+@Composable
+fun <T> DropItem(
+    modifier: Modifier,
+    content: @Composable() (BoxScope.(isInBound: Boolean, data: T?) -> Unit),
+) {
+    val dragInfo = LocalDragTargetInfo.current
+    val dragPosition = dragInfo.dragPosition
+    val dragOffset = dragInfo.dragOffset
+    var isCurrentDropTarget by remember {
+        mutableStateOf(false)
+    }
+
+    Box(
+        modifier = modifier
+            .width(135.dp)
+            .height(40.dp)
+            .onGloballyPositioned {
+                it
+                    .boundsInWindow()
+                    .let { rect ->
+                        isCurrentDropTarget = rect.contains(dragPosition + dragOffset)
+                    }
+            }
+    ) {
+        val data = if(isCurrentDropTarget && !dragInfo.isDragging) dragInfo.dataDrop as T? else null
+        content(isCurrentDropTarget, data)
+    }
+}
+
+@Composable
+fun DragableScreen(
+    modifier: Modifier = Modifier,
+    content: @Composable BoxScope.() -> Unit
+) {
+    val state = remember {
+        DragAndDrop()
+    }
+
+    CompositionLocalProvider(
+        LocalDragTargetInfo provides state
+    ) {
+        Box(modifier = modifier.fillMaxSize()) {
+            content()
+            if (state.isDragging) {
+                var targetSize by remember {
+                    mutableStateOf(IntSize.Zero)
+                }
+
+                Box(
+                    modifier = Modifier
+                        .graphicsLayer {
+                            val offset = (state.dragPosition + state.dragOffset)
+                            scaleX = 1.3f
+                            scaleY = 1.3f
+                            alpha = if (targetSize == IntSize.Zero) 0f else .9f
+                            translationX = offset.x.minus(targetSize.width / 2)
+                            translationY = offset.y.minus(targetSize.height / 2)
+                        }
+                        .onGloballyPositioned {
+                            targetSize = it.size
+                        }
+                ) {
+                    state.draggableComposable?.invoke()
+                }
+            }
+        }
+    }
+}
+
+
+class DragAndDrop {
     var isDragging: Boolean by mutableStateOf(false)
     var dragPosition by mutableStateOf(Offset.Zero)
     var dragOffset by mutableStateOf(Offset.Zero)

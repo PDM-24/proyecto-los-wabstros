dcbb31f898918d5d4b76ac48857ce6cf43df954c
EliasZelaya <00146322@uca.edu.sv>
2024-06-22 00:58:15 -0600

    modify dataquiz

--- app/src/main/java/com/example/codelesson/data/DataQuiz.kt
+++ app/src/main/java/com/example/codelesson/data/DataQuiz.kt
@@ -9,15 +9,15 @@ fun quizList() : List<QuizData> {
     return listOf(
         QuizData(
             "Question 1",
-            "Answer 1"
+            "Numero\nDecimal"
         ),
         QuizData(
             "Question 2",
-            "Answer 2"
+            "Numero\nEntero"
         ),
         QuizData(
             "Question 3",
-            "Answer 3"
+            "Caracter"
         ),
     )
 }

84648cb5fd54dd4052dc4f4f6c2ed5a79055bcb0
EliasZelaya <00146322@uca.edu.sv>
2024-06-23 16:31:56 -0600

    Add api constants tu routes

---
+++ app/src/main/java/com/example/codelesson/util/Constants.kt
@@ -0,0 +1,16 @@
+package com.example.codelesson.util
+
+object Constants {
+    const val BASE_URL = "http://192.168.0.3:8080/api/"
+    const val AUTH_CONSTANT = "auth"
+    const val POST_USER = "/register"
+    const val GET_USER = "/login"
+    const val USER_CONSTANT = "user"
+    const val UPDATE_USER_PASSWORD = "/update-password"
+    const val UPDATE_USER_PROFILE = "/update-profile"
+    const val GET_ALL_USERS = "/all"
+    const val LESSON_CONSTANT = "lesson"
+    const val GET_ALL_LESSONS = "/all"
+    const val GET_LESSON_TITLE = "/allLessons"
+    const val GET_LESSON = "/get"
+}

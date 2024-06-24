0803299a41d51d57381b152590ed1ddeea9a67d4
EliasZelaya <00146322@uca.edu.sv>
2024-06-24 13:47:19 -0600

    Changes in apiservice

--- app/src/main/java/com/example/codelesson/data/remote/api/ApiService.kt
+++ app/src/main/java/com/example/codelesson/data/remote/api/ApiService.kt
@@ -14,6 +14,7 @@ import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface ApiService {
+    //Makes the login request and returns a token which can be used to bring the user's data
     @POST(Constants.AUTH_CONSTANT + Constants.LOG_USER)
     suspend fun auth(
         @Body userDataLogin: UserDataLogin
--- app/src/main/java/com/example/codelesson/util/UserViewModel.kt
+++
@@ -1,6 +0,0 @@
-package com.example.codelesson.util
-
-import androidx.lifecycle.ViewModel
-
-class UserViewModel : ViewModel() {
-}

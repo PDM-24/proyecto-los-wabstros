ebdd6d6f8aa71a40e70cec1235e9ff878903b71c
EliasZelaya <00146322@uca.edu.sv>
2024-06-24 11:36:42 -0600

    Adding more constants to api calls

--- app/src/main/java/com/example/codelesson/util/Constants.kt
+++ app/src/main/java/com/example/codelesson/util/Constants.kt
@@ -1,11 +1,12 @@
 package com.example.codelesson.util
 
 object Constants {
-    const val BASE_URL = "http://192.168.0.3:8080/api/"
+    const val BASE_URL = "http://192.168.198.21:8080/api/"
     const val AUTH_CONSTANT = "auth"
     const val POST_USER = "/register"
-    const val GET_USER = "/login"
+    const val LOG_USER = "/login"
     const val USER_CONSTANT = "user"
+    const val GET_USER_PROFILE = "/profile"
     const val UPDATE_USER_PASSWORD = "/update-password"
     const val UPDATE_USER_PROFILE = "/update-profile"
     const val GET_ALL_USERS = "/all"

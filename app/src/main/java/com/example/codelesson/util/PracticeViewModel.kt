9cb92c2775ab011908f5920005df9554e086e05b
EliasZelaya <00146322@uca.edu.sv>
2024-06-24 23:20:47 -0600

    Add functions to update and get Exp user

--- app/src/main/java/com/example/codelesson/util/PracticeViewModel.kt
+++ app/src/main/java/com/example/codelesson/util/PracticeViewModel.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.codelesson.data.models.ExpUpdateData
 import com.example.codelesson.data.models.LessonData
 import com.example.codelesson.data.models.TitleLesson
 import com.example.codelesson.data.remote.api.ApiClient
@@ -33,6 +34,10 @@ class PracticeViewModel : ViewModel() {
         )
     )
     val getLesson = _getLesson.asStateFlow()
+    private val _token = MutableStateFlow("")
+    val token = _token.asStateFlow()
+    private val _exp = MutableStateFlow(0)
+    val exp = _exp.asStateFlow()
 
     //When start the app, init var titleList
     init {
@@ -72,6 +77,36 @@ class PracticeViewModel : ViewModel() {
                 _getLesson.value = response.data
             } catch (e: Exception) {
                 Log.d("Get Lesson", e.message.toString())
+            }
+        }
+    }
+    //Funcion para obtener el token desde el UserViewModel
+    fun getToken(token: String) {
+        _token.value = token
+    }
+
+    fun getExp() {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                val response = api.getExp("Bearer ${_token.value}")
+                _exp.value = response.data.exp
+                Log.d("Exp", _exp.value.toString())
+            }
+            catch (e: Exception) {
+                Log.d("Exp", e.message.toString())
+            }
+        }
+    }
+    //El parametro sera la nueva exp, la cual se calculara en la screen de congrats
+    //y se le enviara como parametro
+    fun updateExp(newExp: ExpUpdateData) {
+        viewModelScope.launch {
+            try {
+                val response = api.updateExp(newExp, "Bearer ${_token.value}")
+                _token.value = response.data.token
+            }
+            catch (e: Exception) {
+                Log.d("Update Exp", e.message.toString())
             }
         }
     }

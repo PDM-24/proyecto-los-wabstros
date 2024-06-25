5dbbd60f275ec0aeb0967c099b9a11631e4c323b
ferroa <00300722@uca.edu.sv>
2024-06-25 05:24:16 -0600

    feat: edit profile logic

--- app/src/main/java/com/example/codelesson/ui/screens/EditProfile.kt
+++ app/src/main/java/com/example/codelesson/ui/screens/EditProfile.kt
@@ -1,8 +1,8 @@
 package com.example.codelesson.ui.screens
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
@@ -14,17 +14,23 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.TextStyle
@@ -32,13 +38,21 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
 import com.example.codelesson.R
 import com.example.codelesson.ui.components.profileComponents.mainLetters
+import com.example.codelesson.ui.components.profileComponents.message
 import com.example.codelesson.ui.theme.DarkGrey
 import com.example.codelesson.util.UserViewModel
 
+var nombreUsuario: String = ""
+var apellidoUsuario: String = ""
+var emailUsuario: String = ""
+
+private val emptyFieldText: MutableState<Boolean> = mutableStateOf(false)
 @Composable
-fun EditProfile(innerPadding: PaddingValues, viewModel: UserViewModel) {
+fun EditProfile(innerPadding: PaddingValues, viewModel: UserViewModel, navController: NavHostController) {
+    val state by viewModel.user.collectAsState()
     Column (
         modifier = Modifier
             .padding(innerPadding)
@@ -48,6 +62,8 @@ fun EditProfile(innerPadding: PaddingValues, viewModel: UserViewModel) {
             .background(DarkGrey),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
+
+        val context = LocalContext.current
             Box (
                 modifier = Modifier
                     .padding(8.dp)
@@ -75,32 +91,66 @@ fun EditProfile(innerPadding: PaddingValues, viewModel: UserViewModel) {
 
         Spacer(modifier = Modifier.height(20.dp))
 
-        profileTexField("Chistina", "Nombre")
+        profileTexField(state.nombre, "Nombre")
 
-        profileTexField("Salazar", "Apellido" )
+        profileTexField(state.apellido, "Apellido" )
 
-        profileTexField("alesalazar@gmail.com", "Correo electronico" )
+        profileTexField(state.email, "Correo electronico" )
 
         Spacer(modifier = Modifier.height(75.dp))
 
         profilBbutton("Actualizar") {
-            
+            if (nombreUsuario != "" && apellidoUsuario != "" && emailUsuario != ""){
+                viewModel.updateUser(nombreUsuario, apellidoUsuario, emailUsuario)
+                message(context, "Datos actualizados")
+                navController.navigate("Profile")
+            }else{
+                emptyFieldText.value = false
+                message(context, "Complete todos los datos")
+            }
         }
     }
-
 }
 
 @Preview
 @Composable
 fun EditProfilePreview() {
-    EditProfile(innerPadding = PaddingValues(0.dp), viewModel = UserViewModel())
+    EditProfile(
+        innerPadding = PaddingValues(0.dp),
+        viewModel = UserViewModel(),
+        navController = NavHostController(LocalContext.current)
+    )
 }
 
 @Composable
-fun profileTexField(name : String, label : String){
+fun profileTexField(contenido : String, label : String){
+    var valor by remember {
+        mutableStateOf(contenido)
+    }
+        if (label == "Nombre"){
+
+            if (valor != ""){
+                nombreUsuario = valor
+            }else{
+                nombreUsuario = ""
+            }
+        }else if (label == "Apellido"){
+            if (valor != ""){
+                apellidoUsuario = valor
+            }else{
+                apellidoUsuario = ""
+            }
+        }else if (label == "Correo electronico"){
+            if (valor != ""){
+                emailUsuario = valor
+            }else{
+                emailUsuario = ""
+            }
+        }
+
     Spacer(modifier = Modifier.height(20.dp))
     TextField(
-        value = name,
+        value = valor,
         textStyle = TextStyle(color = Color.Black, fontSize = 16.sp, fontWeight = FontWeight.SemiBold),
         label = { Text(text = label) },
         modifier = Modifier
@@ -112,6 +162,6 @@ fun profileTexField(name : String, label : String){
             focusedTextColor = Color.Black,
             unfocusedTextColor = Color.Black
         ),
-        onValueChange = {}
+        onValueChange = {valor = it}
     )
 }

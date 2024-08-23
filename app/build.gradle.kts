32046b2be9cfdec1a14bfbe251cd08e32dac2f2c
EliasZelaya <00146322@gmail.com>
2024-08-23 16:32:59 -0600

    feat: add dependency to use hiltViewModel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     //Dagger-Hilt
     implementation ("com.google.dagger:hilt-android:2.52")
     kapt("com.google.dagger:hilt-compiler:2.52")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
